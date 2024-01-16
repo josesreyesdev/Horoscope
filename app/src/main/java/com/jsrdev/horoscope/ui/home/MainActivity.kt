304135a99a258d438888f1e725bc7a973867f72b
Jose SR <josesanchez09190@gmail.com>
2024-01-16 02:03:05 -0600

    upgraded bottom navigation view

--- app/src/main/java/com/jsrdev/horoscope/ui/home/MainActivity.kt
+++ app/src/main/java/com/jsrdev/horoscope/ui/home/MainActivity.kt
@@ -2,15 +2,33 @@ package com.jsrdev.horoscope.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jsrdev.horoscope.R
 import com.jsrdev.horoscope.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
