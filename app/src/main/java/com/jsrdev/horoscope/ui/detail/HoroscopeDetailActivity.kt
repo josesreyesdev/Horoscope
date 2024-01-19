7a2dad2c79943853fafbfa5b70d3c8d05c578140
Jose SR <josesanchez09190@gmail.com>
2024-01-19 02:14:46 -0600

    added horoscope detail design

--- app/src/main/java/com/jsrdev/horoscope/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/jsrdev/horoscope/ui/detail/HoroscopeDetailActivity.kt
@@ -6,10 +6,16 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.navArgs
 import com.jsrdev.horoscope.R
 import com.jsrdev.horoscope.databinding.ActivityHoroscopeDetailBinding
-import com.jsrdev.horoscope.ui.home.HoroscopeDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -17,6 +23,8 @@ class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHoroscopeDetailBinding
 
     private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
+
+    private val args: HoroscopeDetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +36,36 @@ class HoroscopeDetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initUi()
+    }
+
+    private fun initUi() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect { state ->
+                    when(state) {
+                        is HoroscopeDetailState.Error -> errorState()
+                        HoroscopeDetailState.Loading -> loadingState(true)
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState(state: Boolean) {
+        binding.progressBar.isVisible = state
+    }
+
+    private fun errorState() {
+    }
+
+    private fun successState() {
+        loadingState(false)
     }
 }
--- app/src/main/java/com/jsrdev/horoscope/ui/home/HoroscopeDetailViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jsrdev.horoscope.ui.home
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
-}
